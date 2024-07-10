ecebb4df0cb500c3c226a39bb6289cdf6c1f20b7
samuel <skifle507@gmail.com>
2024-07-10 15:50:58 +0300

    adding all UserController routwe methods

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,51 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  Body,
+  Delete,
+  Patch,
+  Query,
+} from '@nestjs/common';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  /*
+  GET /users or query parameter
+  GET /users/:id
+  POST /users
+  PATCH /users/:id
+  DELETE /users/:id
+  */
+
+  @Get()
+  findAll(@Query('role') role?: 'ENTERN' | 'ENGINEER' | 'ADMIN') {
+    return [];
+  }
+
+  @Get('enternships')
+  findAllEnternships() {
+    return [];
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return { id };
+  }
+
+  @Post()
+  create(@Body() user: {}) {
+    return user;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() userUpdate: {}) {
+    return [];
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return { id };
+  }
+}
