d2b602c9ca58e0f660a4c8bb350f4497c37eac1a
samuel <skifle507@gmail.com>
2024-07-10 16:34:56 +0300

    adding UserService methods

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -28,4 +28,59 @@ export class UserService {
       role: 'ENTERN',
     },
   ];
+
+  // we will create a methods for userController
+
+  findAll(role?: 'ENTERN' | 'ENGINEER' | 'ADMIN') {
+    if (role) {
+      return this.users.filter((user) => user.role === role);
+    }
+
+    return this.users;
+  }
+
+  findOne(id: number) {
+    const user = this.users.find((user) => user.id === id);
+    return user;
+  }
+
+  create(user: {
+    name: string;
+    email: string;
+    role: 'ENTERN' | 'ENGINEER' | 'ADMIN';
+  }) {
+    userByHighestId = [...this.users].sort((a, b) => (a.id = b.id));
+
+    const newUser = {
+      id: userByHighestId[0] + 1,
+      ...user,
+    };
+
+    this.users.push(newUser);
+    return newUser;
+  }
+
+  update(
+    id: number,
+    updatedUser: {
+      name?: string;
+      email?: string;
+      role?: 'ENTERN' | 'ENGINEER' | 'ADMIN';
+    },
+  ) {
+    this.users = this.users.map((user) => {
+      if (user.id === id) {
+        return [...user, ...updatedUser];
+      }
+      return user;
+    });
+
+    return this.findOne(id);
+  }
+
+  delete(id: number) {
+    const removedUser = this.findOne(id);
+    this.users = this.users.filter((user) => user.id !== id);
+    return removedUser;
+  }
 }
