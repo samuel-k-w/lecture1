fa8a20f904936dba01ea76c3219c85f8cb2fe6be
samuel <skifle507@gmail.com>
2024-07-10 15:56:19 +0300

    adding users data

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,31 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  private users = [
+    {
+      id: 1,
+      name: 'name1',
+      email: 'email1',
+      role: 'ENTERN',
+    },
+    {
+      id: 2,
+      name: 'name2',
+      email: 'email2',
+      role: 'ENTERN',
+    },
+    {
+      id: 3,
+      name: 'name3',
+      email: 'email3',
+      role: 'ENTERN',
+    },
+    {
+      id: 4,
+      name: 'name4',
+      email: 'email4',
+      role: 'ENTERN',
+    },
+  ];
+}
