1826246706226fff1de85fce1f8a9039285fa8f1
samuel <skifle507@gmail.com>
2024-07-12 09:06:37 +0300

    adding and configuring database to use CRUD API

---
+++ prisma/migrations/20240712051551_name_type_id/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Employee" ADD CONSTRAINT "Employee_pkey" PRIMARY KEY ("id");
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,35 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { DatabaseModule } from './database/database.module';
+import { EmployeesModule } from './employees/employees.module';
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-  imports: [UserModule, DatabaseModule],
+  imports: [
+    UserModule,
+    DatabaseModule,
+    EmployeesModule,
+    ThrottlerModule.forRoot([
+      {
+        name: 'short',
+        ttl: 1000,
+        limit: 3,
+      },
+      {
+        name: 'long',
+        ttl: 60000,
+        limit: 100,
+      },
+    ]),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
